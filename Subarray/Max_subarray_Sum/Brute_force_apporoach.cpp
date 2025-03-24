7f70c05aeb4a27cc099c91be3e6065006c04ac95
DhruvinXDev <dhruvinvaghasiya297@gmail.com>
2025-03-25 02:41:36 +0530

    Re-added Subarray folder

---
+++ Subarray/Max_subarray_Sum/Brute_force_apporoach.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main ( )
+{
+    int n = 9;
+    int arr[ ] = {-2,1,-3,4,-1,2,1,-5,4};
+    int max_sum = arr[0];
+    //time cmp is (n ^ 2)
+    for(int start=0;start<n;start++)
+    {
+        int current_sum =0;
+        for(int i=start;i<n;i++)
+        {
+            current_sum += arr[i];
+            max_sum = max(current_sum,max_sum);
+        }
+    }
+
+    cout<<"Max sum is = "<<max_sum;
+    return 0;
+
+}
